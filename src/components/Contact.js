25e0a5d0d583d6d6107234d71743e4e5e0059c1a
Emirlan <abdykadyrov.emirlan.00@gmail.com>
2023-08-12 21:32:05 +0600

    contact update

---
+++ src/components/Contact.js
@@ -0,0 +1,50 @@
+import email from "../media/icons/email.png";
+import phone from "../media/icons/phone.png";
+
+export default function Contact() {
+  const handleSubmit = (event) => {
+    console.log("handleSubmit ran");
+    event.preventDefault();
+    document.form.reset();
+  };
+  return (
+    <>
+      <div className="section contact" id="contact">
+        <div className="h2-bg">CONTACT</div>
+        <h2>Get in Touch</h2>
+        <div className="contact-info">
+          <div className="contact-form">
+            <span>Contact Me Now</span>
+            <form name="form">
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Phone or Email" />
+
+              <textarea placeholder="Tell me more about your needs......"></textarea>
+              <div className="button-wrapper">
+                <button onClick={handleSubmit} className="button buttonFill">
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="sep">or</div>
+          <div className="contact-later">
+            <span>Contact Me Later</span>
+            <div>
+              <span>
+                <img src={email} alt="Email Icon" />
+                emirlan9191@gmail.com
+              </span>
+            </div>
+            <div>
+              <span>
+                <img src={phone} alt="Phone Icon" />
+                +996 550 01 37 34
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
