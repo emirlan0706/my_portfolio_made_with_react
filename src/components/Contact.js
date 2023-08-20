5d109aa507568540dd8b6b288f565200233857b8
Emirlan <abdykadyrov.emirlan.00@gmail.com>
2023-08-20 20:21:52 +0600

    contact coments

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,52 +1,52 @@
-// import email from "../media/icons/email.png";
-// import phone from "../media/icons/phone.png";
+import email from "../media/icons/email.png";
+import phone from "../media/icons/phone.png";
 
-// export default function Contact() {
-//   const handleSubmit = (event) => {
-//     console.log("handleSubmit ran");
-//     event.preventDefault();
-//     document.form.reset();
-//   };
-//   return (
-//     <>
-//       <div className="section contact" id="contact">
-//         <div className="h2-bg">CONTACT</div>
-//         <h2>Get in Touch</h2>
-//         <div className="contact-info">
-//           <div className="contact-form">
-//             <span>Contact Me Now</span>
-//             <form name="form">
-//               <input type="text" placeholder="Name" />
-//               <input type="text" placeholder="Phone or Email" />
+export default function Contact() {
+  const handleSubmit = (event) => {
+    console.log("handleSubmit ran");
+    event.preventDefault();
+    document.form.reset();
+  };
+  return (
+    <>
+      <div className="section contact" id="contact">
+        <div className="h2-bg">CONTACT</div>
+        <h2>Get in Touch</h2>
+        <div className="contact-info">
+          <div className="contact-form">
+            <span>Contact Me Now</span>
+            <form name="form">
+              <input type="text" placeholder="Name" />
+              <input type="text" placeholder="Phone or Email" />
 
-//               <textarea placeholder="Tell me more about your needs......"></textarea>
-//               <div className="button-wrapper">
-//                 {/* react  */}
+              <textarea placeholder="Tell me more about your needs......"></textarea>
+              <div className="button-wrapper">
+                {/* react  */}
 
-//                 <button onClick={handleSubmit} className="button buttonFill">
-//                   Send Message
-//                 </button>
-//               </div>
-//             </form>
-//           </div>
-//           <div className="sep">or</div>
-//           <div className="contact-later">
-//             <span>Contact Me Later</span>
-//             <div>
-//               <span>
-//                 <img src={email} alt="Email Icon" />
-//                 emirlan9191@gmail.com
-//               </span>
-//             </div>
-//             <div>
-//               <span>
-//                 <img src={phone} alt="Phone Icon" />
-//                 +996 550 01 37 34
-//               </span>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
+                <button onClick={handleSubmit} className="button buttonFill">
+                  Send Message
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="sep">or</div>
+          <div className="contact-later">
+            <span>Contact Me Later</span>
+            <div>
+              <span>
+                <img src={email} alt="Email Icon" />
+                emirlan9191@gmail.com
+              </span>
+            </div>
+            <div>
+              <span>
+                <img src={phone} alt="Phone Icon" />
+                +996 550 01 37 34
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
